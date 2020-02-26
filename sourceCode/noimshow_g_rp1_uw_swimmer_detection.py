322ce7d3fd613cc06cf0a432c1083e0936179d7f
aldenkane <akane2@nd.edu>
2020-02-26 11:34:55 -0500

    Fixing Spelling Errors

--- sourceCode/noimshow_g_rp1_uw_swimmer_detection.py
+++ sourceCode/noimshow_g_rp1_uw_swimmer_detection.py
@@ -26,7 +26,7 @@ from decimal import *
 # Section 0.1: Use Argparse for Command Line Arguements
 #######################################################
 parser = argparse.ArgumentParser()
-parser.add_arguement('-i', "--ip",    type=str,   default=0,  help="IP/TCP Address of Server/Laptop to Send Images To", required=True)
+parser.add_argument('-i', "--ip",    type=str,   default=0,  help="IP/TCP Address of Server/Laptop to Send Images To", required=True)
 args = parser.parse_known_args()
 
 ip_address = args.ip

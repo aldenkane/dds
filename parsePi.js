6555874409c899c7ddc31c89b321c9c1acf62941
James Murdock Graham <51002460+james-murdock-graham@users.noreply.github.com>
2020-03-19 18:41:37 -0400

    Update parsePi.js

--- parsePi.js
+++ parsePi.js
@@ -1,21 +1,22 @@
-//Import parse module
+//Import parse and btoa modules
 const Parse = require('parse/node');
+const btoa = require('btoa')
 
 //Initialize Parse server
 Parse.serverURL = 'https://optoswim.back4app.io'; // This is your Server URL
 Parse.initialize(
-  '03Pq0kbLRvci8D3OV92OFIIbNidw3kZrGma2sruS', // This is your Application ID
-  'SMnIF1sMs1zdczYwWU1SikdLtfIu4IzcWYhBhEMf', // This is your Javascript key
-  'HSNUMKsUeTtNjTxOIPB2ct3FIiD6NMJp7yc5w9WW' // This is your Master key (never use it in the frontend)
+  '03Pq0kbLRvci8D3OV92OFIIbNidw3kZrGma2sruS', // Application ID
+  'SMnIF1sMs1zdczYwWU1SikdLtfIu4IzcWYhBhEMf', // Javascript key
+  'HSNUMKsUeTtNjTxOIPB2ct3FIiD6NMJp7yc5w9WW' // Master key (never to be used in production on clientside)
 );
 
 //Create object in Image class linked to specific user
 
-function sendImage(swimmerDetected, numberSwimmers, drowningDetected){
+function sendImage(fileName, swimmerDetected, numberSwimmers, drowningDetected){
   const Images = Parse.Object.extend('Images');
   const image = new Images();
 
-  image.set('image', new Parse.File("lastFrame.jpg", { base64: btoa("My file content") }));
+  image.set('image', new Parse.File(fileName, { base64: btoa(fileName) }));
   image.set('swimmerDetected', swimmerDetected);
   image.set('numberSwimmers', numberSwimmers);
   image.set('Emergency', drowningDetected);

0c7bfaba18cef88b4161f39f448938cfe7266b77
James Murdock Graham <51002460+james-murdock-graham@users.noreply.github.com>
2020-04-15 12:52:28 -0400

    Update ParsePi.js

--- parsePi.js
+++ parsePi.js
@@ -1,41 +1,35 @@
-//Import parse and btoa modules
 const Parse = require('parse/node');
 const btoa = require('btoa');
 const fs = require('fs');
 
-//Initialize Parse server
 Parse.serverURL = 'https://optoswim.back4app.io'; // This is your Server URL
 Parse.initialize(
-  '03Pq0kbLRvci8D3OV92OFIIbNidw3kZrGma2sruS', // Application ID
-  'SMnIF1sMs1zdczYwWU1SikdLtfIu4IzcWYhBhEMf', // Javascript key
-  'HSNUMKsUeTtNjTxOIPB2ct3FIiD6NMJp7yc5w9WW' // Master key (never to be used in production on clientside)
+  '03Pq0kbLRvci8D3OV92OFIIbNidw3kZrGma2sruS', // This is your Application ID
+  'SMnIF1sMs1zdczYwWU1SikdLtfIu4IzcWYhBhEMf', // This is your Javascript key
+  'HSNUMKsUeTtNjTxOIPB2ct3FIiD6NMJp7yc5w9WW' // This is your Master key (never use it in the frontend)
 );
 
-//Create object in Image class linked to specific user
-const sendImage = (imgFilePath) => {
-    try{
-        fs.watch(imgFilePath, (event, filename) => {
-            if (filename){
-            console.log(`${filename} file changed`)
-            //Load JSON file into javscript object
-            //let obj = JSON.parse(fs.readFileSync(jsonFilePath))
-            const Images = Parse.Object.extend('Images');
-            const image = new Images();
-            //const imgFilePath = obj.imageFilePath
-            image.set('image', new Parse.File(imgFilePath, { base64: btoa(imgFilePath) }));
-            // image.set('swimDetected', obj.swimmerDetected);
-            //image.set('numberSwimmers', obj.numberSwimmers);
-            //image.set('drownDetected', obj.drowningDetected);
-            //image.set('user', Parse.User.current());
-            image.save()
-            }
-        })
-    } catch (ex) {
-        console.log(ex);
-    }
-};
- 
-sendImage('./last_Image/last_Frame.jpg');
- 
+const Images = Parse.Object.extend('Images');
+const image = new Images();
 
-sendImage('./last_Image/last_Frame.jpg');
+image.set('swimDetected', true);
+image.set('numberSwimmers', 1);
+image.set('drownDetected', true);
+image.set(
+  'image',
+  new Parse.File('resume.txt', { base64: btoa('resume.txt') })
+);
+
+image.save().then(
+  (result) => {
+    if (typeof document !== 'undefined')
+      document.write(`Images created: ${JSON.stringify(result)}`);
+    console.log('Images created', result);
+  },
+  (error) => {
+    if (typeof document !== 'undefined')
+      document.write(`Error while creating Images: ${JSON.stringify(error)}`);
+    console.error('Error while creating Images: ', error);
+  }
+);
+

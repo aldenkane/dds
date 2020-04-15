894c60bac6e956c7f7e56981e9dbec5b6134381d
James Murdock Graham <51002460+james-murdock-graham@users.noreply.github.com>
2020-04-15 15:34:10 -0400

    Update parsePi.js

--- parsePi.js
+++ parsePi.js
@@ -10,14 +10,14 @@ Parse.initialize(
 );
 
 function send(swimDetected, numberSwimmers, drownDetected) {
-  let file = fs.readFileSync('last_Frame.png');
+  let file = fs.readFileSync('last_Frame.jpg');
   const Images = Parse.Object.extend('Images');
   const image = new Images();
 
   image.set('swimDetected', swimDetected);
   image.set('numberSwimmers', numberSwimmers);
   image.set('drownDetected', drownDetected);
-  image.set('image', new Parse.File('last_Frame.png', { base64: btoa(file) }));
+  image.set('image', new Parse.File('last_Frame.jpg', { base64: btoa(file) }));
 
   image.save().then(
     (result) => {

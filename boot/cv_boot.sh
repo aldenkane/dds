22ae6e1bfdadda50f77069fdbd86a0c66ce67965
aldenkane <akane2@nd.edu>
2020-04-22 11:11:21 -0400

    New boot services

---
+++ boot/cv_boot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/pi/dds/sourceCode
+/usr/bin/python3 start_rpi_uw_swimmer_detection.py
---
+++ boot/node_boot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/pi/dds/sourceCode
+/usr/bin/local/node parsePi.js

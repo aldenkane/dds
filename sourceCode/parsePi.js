cb9bd5124250ed82f365a4cee19dbc2e3022e425
james-murdock-graham <51002460+james-murdock-graham@users.noreply.github.com>
2020-05-23 13:59:39 -0400

    Added event and livefeed subscriptions and conditional streams being read on client side

--- sourceCode/parsePi.js
+++ sourceCode/parsePi.js
@@ -2,14 +2,16 @@ const Parse = require('parse/node');
 const btoa = require('btoa');
 const fs = require('fs');
 
-Parse.serverURL = 'https://optoswim.back4app.io'; // This is your Server URL
+//Initializes Parse Object
+Parse.serverURL = 'https://optoswim.back4app.io'; // Server URL
 Parse.initialize(
-  '03Pq0kbLRvci8D3OV92OFIIbNidw3kZrGma2sruS', // This is your Application ID
-  'SMnIF1sMs1zdczYwWU1SikdLtfIu4IzcWYhBhEMf', // This is your Javascript key
-  'HSNUMKsUeTtNjTxOIPB2ct3FIiD6NMJp7yc5w9WW' // This is your Master key (never use it in the frontend)
+  '03Pq0kbLRvci8D3OV92OFIIbNidw3kZrGma2sruS', // Application ID
+  'SMnIF1sMs1zdczYwWU1SikdLtfIu4IzcWYhBhEMf', // Javascript key
+  'HSNUMKsUeTtNjTxOIPB2ct3FIiD6NMJp7yc5w9WW' // Master key (never use it in the frontend)
 );
 
-function send(swimDetected, numberSwimmers, drownDetected, serialNo) {
+// Creates an Image object in Parse DB
+function sendImage(swimDetected, numberSwimmers, drownDetected, serialNo) {
   let file = fs.readFileSync('../last_Image/last_Frame.jpg');
   const Images = Parse.Object.extend('Images');
   const image = new Images();
@@ -33,17 +35,81 @@ function send(swimDetected, numberSwimmers, drownDetected, serialNo) {
     }
   );
 }
-// const options = {
-//   persistent: true,
-// }
+
+//Creates an Events Object in Parse DB
+function sendEvent(swimDetected, numberSwimmers, drownDetected, serialNo) {
+  let file = fs.readFileSync('../last_Image/last_Frame.jpg');
+  const Events = Parse.Object.extend('Events');
+  const event = new Events();
+
+  event.set('swimDetected', swimDetected);
+  event.set('numberSwimmers', numberSwimmers);
+  event.set('drownDetected', drownDetected);
+  event.set('serialNo', serialNo);
+  event.set('image', new Parse.File('last_Frame.jpg', { base64: btoa(file) }));
+
+
+  event.save().then(
+    (result) => {
+      if (typeof document !== 'undefined')
+        document.write(`Images created: ${JSON.stringify(result)}`);
+      console.log('Images created', result);
+    },
+    (error) => {
+      if (typeof document !== 'undefined')
+        document.write(`Error while creating Images: ${JSON.stringify(error)}`);
+      console.error('Error while creating Images: ', error);
+    }
+  );
+}
+
+
+// constantly watches and creates Events and Image object when detected
 fs.watch('../last_Image/event.json', (event, filename) => {
+  let prevEvent, currEvent;
   if (filename) {
     let jsObj = require('../last_Image/event.json');
-    send(
-      jsObj.swimDetected,
-      parseInt(jsObj.numberSwimmers),
-      jsObj.drownDetected,
-      jsObj.serialNo
-    );
+    if (jsObj.swimDetected === true || jsObj.drownDetected === true && prevEvent !== jsObj) {
+      sendEvent(
+        jsObj.swimDetected,
+        parseInt(jsObj.numberSwimmers),
+        jsObj.drownDetected,
+        jsObj.serialNo
+      )
+      prevEvent = jsObj
+    }
   }
 });
+
+// Starts watching and creating Images objects when a LiveFeed object is created
+function liveQuery() {
+  var client = await new Parse.LiveQueryClient({
+    applicationId: '03Pq0kbLRvci8D3OV92OFIIbNidw3kZrGma2sruS',
+    serverURL: 'wss://' + 'optoswim.back4app.io', // Example: 'wss://livequerytutorial.back4app.io'
+    javascriptKey: 'SMnIF1sMs1zdczYwWU1SikdLtfIu4IzcWYhBhEMf',
+    masterKey: 'HSNUMKsUeTtNjTxOIPB2ct3FIiD6NMJp7yc5w9WW',
+  })
+  await client.open()
+
+  // Creates a new Query object to help us fetch MyCustomClass objects
+  const query = new Parse.Query('LiveFeed')
+  query.equalTo('liveFeed', true)
+
+  var subscription = await client.subscribe(query)
+
+  subscription.on('create', async () => {
+    fs.watch('../last_Image/event.json', (event, filename) => {
+      if (filename) {
+        let jsObj = require('../last_Image/event.json');
+        sendImage(
+          jsObj.swimDetected,
+          parseInt(jsObj.numberSwimmers),
+          jsObj.drownDetected,
+          jsObj.serialNo
+        );
+      }
+    });
+  })
+}
+
+liveQuery();
